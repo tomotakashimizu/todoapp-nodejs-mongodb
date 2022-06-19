8bbe7b7f0ca71b957dedc5e63112ad3e913ceaba
tomotakashimizu <tomotaka.developer@gmail.com>
2022-06-20 00:19:26 +0900

    update: get all tasks with axios

--- public/script.js
+++ public/script.js
@@ -1,1 +1,10 @@
-console.log(axios);
+const showTasks = async () => {
+  try {
+    const { data: tasks } = await axios.get('/api/v1/tasks');
+    console.log(tasks);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+showTasks();
