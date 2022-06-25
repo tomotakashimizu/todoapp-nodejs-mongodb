5aac426cfd2856c7e259efc746470f6a1bd2b1d4
tomotakashimizu <tomotaka.developer@gmail.com>
2022-06-25 20:28:21 +0900

    update: display data in HTML

--- public/script.js
+++ public/script.js
@@ -1,7 +1,39 @@
+const tasksDOM = document.querySelector('.tasks');
+
+// /api/v1/tasksからタスクを取得し、表示する
 const showTasks = async () => {
   try {
+    // 自作のAPIを叩いてタスクを取得
     const { data: tasks } = await axios.get('/api/v1/tasks');
     console.log(tasks);
+
+    // タスクを表示
+    const taskNames = tasks
+      .map((task) => {
+        console.log(task);
+        const { _id, name, completed } = task;
+        console.log(name);
+
+        return `
+        <div class="single-task">
+          <h5>
+            <span><i class="far fa-check-circle"></i></span>
+            ${name}
+          </h5>
+          <div class="task-links">
+            <a href="#" class="edit-link">
+              <i class="fas fa-edit"></i>
+            </a>
+            <button type="button" class="delete-btn">
+              <i class="fas fa-trash"></i>
+            </button>
+          </div>
+        </div>`;
+      })
+      .join('');
+
+    console.log(taskNames);
+    tasksDOM.innerHTML = taskNames;
   } catch (error) {
     console.log(error);
   }
