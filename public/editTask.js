3a603c8e6598ccf4687793b166bdbd17c2183839
tomotakashimizu <tomotaka.developer@gmail.com>
2022-07-03 19:23:38 +0900

    update: get a task

--- public/editTask.js
+++ public/editTask.js
@@ -1,4 +1,21 @@
+const taskIDDOM = document.querySelector('.task-edit-id');
+const taskNameDOM = document.querySelector('.task-edit-name');
+
 const params = window.location.search;
 const id = new URLSearchParams(params).get('id');
 
 console.log(id);
+
+const showTask = async () => {
+  try {
+    const { data: task } = await axios.get(`/api/v1/tasks/${id}`);
+    const { _id, name, completed } = task;
+
+    taskIDDOM.textContent = _id;
+    taskNameDOM.value = name;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+showTask();
